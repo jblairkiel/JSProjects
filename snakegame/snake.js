a9d0355613a920b98f3c3eab119b6edd65510f6d
U-CAPS\BBKiel <BBKiel@WINDOWS-8J88I9T.caps.ua.edu>
2017-01-18 17:14:47 -0600

    Working on SnakeGame

---
+++ snakegame/snake.js
@@ -0,0 +1,45 @@
+function Snake() {
+	
+	this.x = 0;
+	this.y = 0;
+	this.xspeed = 1;
+	this.yspeed = 0;
+	this.total = 0;
+	this.tail = [];
+
+	this.dir = function(x, y){
+		this.xspeed = x;
+		this.yspeed = y;
+	}
+
+	this.update = function(){
+		this.x = this.x + this.xspeed *scl;
+		this.y = this.y + this.yspeed *scl;
+
+		this.x = constrain(this.x, 0, width-scl);
+		this.y = constrain(this.y, 0, height-scl);
+
+		for (var i = 0; i< this.total-1; i++){
+			this.tail[i] = this.tail[i+1];
+		}
+		this.tail[this.total-1] = createVector(this.x, this.y);
+	}
+
+	this.show = function(){
+		fill(255);
+		for (var i = 0; i< this.total-1; i++){
+			rect(this.x, this.y, scl, scl);
+		}
+	}
+
+	this.eat = function(pos){
+		this.total++;
+		var d = dist(this.x, this.y, pos.x, pos.y);
+		if(d < 1){
+			return true;
+		}		
+		else{
+			return false;
+		}
+	}
+}
